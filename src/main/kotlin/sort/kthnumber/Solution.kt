9acaf1e12bed0bc1ec62fa7359374de3fba7dabc
youngrock <youngrock.choi@kstec.co.kr>
2021-04-12 22:19:20 +0900

    K번째수 문제

---
+++ src/main/kotlin/sort/kthnumber/Solution.kt
@@ -0,0 +1,8 @@
+package sort.kthnumber
+
+class Solution {
+    fun solution(array: IntArray, commands: Array<IntArray>): IntArray {
+        var answer = intArrayOf()
+        return answer
+    }
+}
