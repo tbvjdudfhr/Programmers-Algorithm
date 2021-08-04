97b0b9e17812ab417ee41424afe20ace07f936fa
youngrock <youngrock.choi@kstec.co.kr>
2021-08-04 13:51:52 +0900

    위클리1주차 수정

--- src/main/java/weekly/calculate/Solution.java
+++ src/main/java/weekly/calculate/Solution.java
@@ -1,20 +1,13 @@
 package weekly.calculate;
 
 class Solution {
-    public long solution(int price, int money, int count)     {
-        long answer = -1;
+    public long solution(int price, int money, int count) {
+        long answer = price * getSum(count) - money;
 
-        answer = money - getSum(price, count);
-
-        if(answer >= 0)
-            return 0;
-        else
-            return answer * -1;
+        return Math.max(answer, 0);
     }
 
-    public static int getSum(int price, int count) {
-        if (count == 1)
-            return price;
-        return price * (count) + getSum(price, count-1);
+    public static long getSum(int n) {
+        return (n * (n + 1)) / 2;
     }
 }
