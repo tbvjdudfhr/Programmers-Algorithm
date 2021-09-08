cee1ead56f311821775d194335acefa5ee8e2e86
youngrock <youngrock.choi@kstec.co.kr>
2021-09-08 23:25:12 +0900

    위클리5주차 문제풀이

---
+++ src/main/java/weekly/_5weeks/Solution.java
@@ -0,0 +1,19 @@
+package weekly._5weeks;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    List<String> list = new ArrayList<>();
+
+    void dfs(String str, int len) {
+        if (len > 5) return;
+        list.add(str);
+        for (int i = 0; i < 5; i++) dfs(str + "AEIOU".charAt(i), len + 1);
+    }
+
+    public int solution(String word) {
+        dfs("", 0);
+        return list.indexOf(word);
+    }
+}
