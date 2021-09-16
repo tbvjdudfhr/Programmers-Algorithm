e96de425de8eff415f4fd931cbbffe9826fce600
youngrock <youngrock.choi@kstec.co.kr>
2021-09-16 11:34:01 +0900

    HashMap,TreeSet 문제풀이

---
+++ src/main/java/inflearn/_4_1/Main.java
@@ -0,0 +1,32 @@
+package inflearn._4_1;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        String line = kb.next();
+        System.out.print(solution(n, line));
+
+        return;
+    }
+
+    private static char solution(int n, String line) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (char x : line.toCharArray()) {
+            map.put(x, map.getOrDefault(x, 0) + 1);
+        }
+
+        int max = Integer.MIN_VALUE;
+        Character answer = 'a';
+        for (char x : map.keySet()) {
+            if (map.get(x) > max) {
+                max = map.get(x);
+                answer = x;
+            }
+        }
+        return answer;
+    }
+}
