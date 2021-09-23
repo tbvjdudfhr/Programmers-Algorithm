2420e0a6ba14d55d2c8ff586c4875d4c6f07cd61
youngrock <youngrock.choi@kstec.co.kr>
2021-09-23 23:03:13 +0900

    최대점수 구하기 문제풀이

---
+++ src/main/java/inflearn/_8_3/Main.java
@@ -0,0 +1,43 @@
+package inflearn._8_3;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    static int n, longTime = 0, maxScore = Integer.MIN_VALUE;
+    static ArrayList<Quiz> quizArrayList = new ArrayList<>();
+
+    public static void DFS(int L, int sumScore, int sumTime, ArrayList<Quiz> list) {
+        if (sumTime > longTime) return;
+        if (L == n) {
+            maxScore = Math.max(maxScore, sumScore);
+        } else {
+            DFS(L + 1, sumScore + list.get(L).score, sumTime + list.get(L).time, list);
+            DFS(L + 1, sumScore, sumTime, list);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        n = kb.nextInt();
+        longTime = kb.nextInt();
+        for (int i = 0; i < n; i++) {
+            quizArrayList.add(new Quiz(kb.nextInt(), kb.nextInt()));
+        }
+        DFS(0, 0, 0, quizArrayList);
+
+        System.out.println(maxScore);
+
+        return;
+    }
+
+    private static class Quiz {
+        int score;
+        int time;
+
+        Quiz(int score, int time) {
+            this.score = score;
+            this.time = time;
+        }
+    }
+}
