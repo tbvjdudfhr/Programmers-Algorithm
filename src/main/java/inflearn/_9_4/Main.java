444a347ae7298b6d9553691fa264a192f2fc9d9f
youngrock <youngrock.choi@kstec.co.kr>
2021-09-29 23:07:03 +0900

    최대수입스케쥴 문제풀이

---
+++ src/main/java/inflearn/_9_4/Main.java
@@ -0,0 +1,53 @@
+package inflearn._9_4;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        ArrayList<Lecture> lectures = new ArrayList<>();
+        int maxDate = 0;
+        for (int i = 0; i < n; i++) {
+            int money = kb.nextInt();
+            int date = kb.nextInt();
+            lectures.add(new Lecture(money, date));
+            maxDate = Math.max(date, maxDate);
+        }
+
+        System.out.println(solution(lectures, maxDate));
+    }
+
+    private static int solution(ArrayList<Lecture> lectures, int maxDate) {
+        Collections.sort(lectures);
+        PriorityQueue<Integer> integerPriorityQueue = new PriorityQueue<>(Comparator.reverseOrder());
+        int answer = 0;
+        int j = 0;
+        for (int i = maxDate; i > 0; i--) {
+            for (; j < lectures.size(); j++) {
+                Lecture lecture = lectures.get(j);
+                if (lecture.date == i) integerPriorityQueue.add(lecture.money);
+                else break;
+            }
+
+            if (!integerPriorityQueue.isEmpty()) answer += integerPriorityQueue.poll();
+        }
+        return answer;
+    }
+
+    private static class Lecture implements Comparable<Lecture> {
+        int money;
+        int date;
+
+        public Lecture(int money, int date) {
+            this.money = money;
+            this.date = date;
+        }
+
+        @Override
+        public int compareTo(Lecture o) {
+            if (this.date == o.date) return o.money - this.money;
+            return o.date - this.date;
+        }
+    }
+}
