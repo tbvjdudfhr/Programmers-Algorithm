ae339f8a94de5abf96be8f737755cd2c1bd860f3
youngrock <youngrock.choi@kstec.co.kr>
2021-10-06 08:54:28 +0900

    계단오르기 문제풀이

---
+++ src/main/java/inflearn/_10_1/Main.java
@@ -0,0 +1,25 @@
+package inflearn._10_1;
+
+import java.util.Scanner;
+
+public class Main {
+    static int[] dp = new int[36];
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        int input = kb.nextInt();
+        solution(input);
+        System.out.println(dp[input]);
+    }
+
+    private static void solution(int input) {
+        for (int i = 1; i <= input; i++) {
+            if(i == 1) {
+                dp[i] = 1;
+            } else if (i == 2) {
+                dp[i] = 2;
+            }else {
+                dp[i] = dp[i -1] + dp[i -2];
+            }
+        }
+    }
+}
