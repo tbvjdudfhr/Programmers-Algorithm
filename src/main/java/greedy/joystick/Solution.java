02f37735457da79f283b3df8708f49e800f8f101
youngrock.choi <tbvjdudfhr@naver.com>
2021-10-23 23:41:59 +0900

    조이스틱

---
+++ src/main/java/greedy/joystick/Solution.java
@@ -0,0 +1,23 @@
+package greedy.joystick;
+
+class Solution {
+    public int solution(String name) {
+        int answer = 0;
+        int len = name.length();
+        int minMove = len - 1;
+
+        for (int i = 0; i < len; i++) {
+            answer += Math.min(name.charAt(i) - 'A', 'Z' - name.charAt(i) + 1);
+
+            int next = i + 1;
+            while (next < len && name.charAt(next) == 'A')
+                next++;
+
+            minMove = Math.min(minMove, i + len - next + i);
+        }
+
+        answer += minMove;
+
+        return answer;
+    }
+}
