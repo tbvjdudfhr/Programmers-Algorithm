11c554d2b3b13d2bad7037d9ddf752f65ca5d1e2
Youngrock Choi <tbvjdudfhr@naver.com>
2022-02-22 23:57:56 +0900

    refactor: stream 변환

--- src/main/java/inflearn/_1_6/Solution.java
+++ src/main/java/inflearn/_1_6/Solution.java
@@ -1,12 +1,13 @@
 package inflearn._1_6;
 
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class Solution {
     public String solution(String str) {
-        String answer = "";
-        for (int i = 0; i < str.length(); i++) {
-            if(str.indexOf(str.charAt(i)) == i) answer += str.charAt(i);
-        }
-
-        return answer;
+        return IntStream.range(0, str.length())
+                .filter(i -> str.indexOf(str.charAt(i)) == i)
+                .mapToObj(i -> String.valueOf(str.charAt(i)))
+                .collect(Collectors.joining());
     }
 }
