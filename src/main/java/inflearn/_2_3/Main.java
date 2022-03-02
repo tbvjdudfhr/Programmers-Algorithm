797e2de00233b4969482ffd1aeb7dd3a7dc13a08
Youngrock Choi <tbvjdudfhr@naver.com>
2022-03-02 23:13:14 +0900

    test: 가위 바위 보

--- src/main/java/inflearn/_2_3/Main.java
+++ src/main/java/inflearn/_2_3/Main.java
@@ -2,18 +2,21 @@ package inflearn._2_3;
 
 
 import java.util.Scanner;
+import java.util.stream.IntStream;
 
 public class Main {
     public String[] solution(int[] a, int[] b) {
-        String[] answer = new String[a.length];
-        for (int i = 0; i < a.length; i++) {
-            if (a[i] - b[i] == 0) answer[i] = "D";
-            else if (a[i] == 1 && b[i] == 3) answer[i] = "A";
-            else if (a[i] == 2 && b[i] == 1) answer[i] = "A";
-            else if (a[i] == 3 && b[i] == 2) answer[i] = "A";
-            else answer[i] = "B";
-        }
-        return answer;
+        return IntStream.range(0, a.length)
+                .mapToObj(index -> getWinner(a[index], b[index]))
+                .toArray(String[]::new);
+    }
+
+    private String getWinner(int a, int b) {
+        if (a - b == 0) return "D";
+        else if (a == 1 && b == 3) return "A";
+        else if (a == 2 && b == 1) return "A";
+        else if (a == 3 && b == 2) return "A";
+        else return "B";
     }
 
     public static void main(String[] args) {
