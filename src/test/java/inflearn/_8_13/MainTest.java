e05571a0e370e91b8ee22b4b931e2ffff1cd1cb7
Youngrock Choi <tbvjdudfhr@naver.com>
2022-04-23 23:21:39 +0900

    test: 섬나라아일랜드

---
+++ src/test/java/inflearn/_8_13/MainTest.java
@@ -0,0 +1,48 @@
+package inflearn._8_13;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class MainTest {
+    private static Stream<Arguments> args() {
+        return Stream.of(
+                Arguments.of(
+                        7,
+                        new int[][]{
+                                {1, 1, 0, 0, 0, 1, 0},
+                                {0, 1, 1, 0, 1, 1, 0},
+                                {0, 1, 0, 0, 0, 0, 0},
+                                {0, 0, 0, 1, 0, 1, 1},
+                                {1, 1, 0, 1, 1, 0, 0},
+                                {1, 0, 0, 0, 1, 0, 0},
+                                {1, 0, 1, 0, 1, 0, 0},
+                        },
+                        5
+                )
+        );
+    }
+
+    @DisplayName("섬나라 아일랜드에 몇개의 섬이 있는지 테스트")
+    @ParameterizedTest
+    @MethodSource("args")
+    void 섬나라_아일랜드(int input, int[][] input2, int result) {
+        Main.board = input2;
+
+        for (int i = 0; i < input; i++) {
+            for (int j = 0; j < input; j++) {
+                if (Main.board[i][j] == 1) {
+                    Main.DFS(i, j);
+                    Main.answer++;
+                }
+            }
+        }
+
+        assertThat(Main.answer).isEqualTo(result);
+    }
+}
