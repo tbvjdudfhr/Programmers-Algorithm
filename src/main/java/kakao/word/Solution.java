d75620d63981ee33ecfe54c6839168a5108994c7
Youngrock Choi <tbvjdudfhr@naver.com>
2022-05-25 22:06:31 +0900

    feat: 숫자 문자열과 영단어

--- src/main/java/kakao/word/Solution.java
+++ src/main/java/kakao/word/Solution.java
@@ -2,7 +2,12 @@ package kakao.word;
 
 public class Solution {
     public int solution(String s) {
-        int answer = 0;
-        return answer;
+        String[] strArr = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
+        for (int i = 0; i < strArr.length; i++) {
+            s = s.replaceAll(strArr[i], Integer.toString(i));
+        }
+
+        return Integer.parseInt(s);
     }
 }
