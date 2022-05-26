e2272a83b9c7abad53bc9c17cfca9a36f0acf3e9
Youngrock Choi <tbvjdudfhr@naver.com>
2022-05-26 22:15:05 +0900

    test: 키패드 누르기

---
+++ src/test/java/kakao/keypad/SolutionTest.java
@@ -0,0 +1,41 @@
+package kakao.keypad;
+
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SolutionTest {
+    private static Stream<Arguments> args() {
+        return Stream.of(
+                Arguments.of(
+                        new int[]{1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5},
+                        "right",
+                        "LRLLLRLLRRL"
+                ),
+                Arguments.of(
+                        new int[]{7, 0, 8, 2, 8, 3, 1, 5, 7, 6, 2},
+                        "left",
+                        "LRLLRRLLLRR"
+                ),
+                Arguments.of(
+                        new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 0},
+                        "right",
+                        "LLRLLRLLRL"
+                )
+        );
+    }
+
+    @DisplayName("신규 유저가 입력한 아이디를 추천 아이디로 변경 테스트")
+    @ParameterizedTest
+    @MethodSource("args")
+    void 신규_아이디_추천(int[] numbers, String hand, String result) {
+        Solution sol = new Solution();
+        assertThat(sol.solution(numbers, hand)).isEqualTo(result);
+    }
+}
