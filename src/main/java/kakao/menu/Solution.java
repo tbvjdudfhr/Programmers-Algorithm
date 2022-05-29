e6b3e492d838d52d9817642dbf93c2e2a3c434ba
Youngrock Choi <tbvjdudfhr@naver.com>
2022-05-29 23:07:39 +0900

    feat: 메뉴 리뉴얼

---
+++ src/main/java/kakao/menu/Solution.java
@@ -0,0 +1,56 @@
+package kakao.menu;
+
+import java.util.*;
+
+public class Solution {
+    List<String> answerList = new ArrayList<>();
+    Map<String, Integer> hashMap = new HashMap<>();
+
+    public String[] solution(String[] orders, int[] course) {
+        for (int i = 0; i < orders.length; i++) {
+            char[] arr = orders[i].toCharArray();
+            Arrays.sort(arr);
+            orders[i] = String.valueOf(arr);
+        }
+
+        for (int courseLength : course) {
+            for (String order : orders) {
+                combination("", order, courseLength);
+            }
+
+            if (!hashMap.isEmpty()) {
+                List<Integer> countList = new ArrayList<>(hashMap.values());
+                int max = Collections.max(countList);
+
+                if (max > 1) {
+                    for (String key : hashMap.keySet()) {
+                        if (hashMap.get(key) == max) {
+                            answerList.add(key);
+                        }
+                    }
+                }
+
+                hashMap.clear();
+            }
+        }
+
+        Collections.sort(answerList);
+        String[] answer = new String[answerList.size()];
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = answerList.get(i);
+        }
+
+        return answer;
+    }
+
+    public void combination(String order, String others, int count) {
+        if (count == 0) {
+            hashMap.put(order, hashMap.getOrDefault(order, 0) + 1);
+            return;
+        }
+
+        for (int i = 0; i < others.length(); i++) {
+            combination(order + others.charAt(i), others.substring(i + 1), count - 1);
+        }
+    }
+}
