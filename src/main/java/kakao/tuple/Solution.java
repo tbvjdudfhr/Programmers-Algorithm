9a34983a291312cf3f43e66af934ff892e3f92f6
Youngrock Choi <tbvjdudfhr@naver.com>
2022-06-05 23:32:36 +0900

    refactor: 튜플

--- src/main/java/kakao/tuple/Solution.java
+++ src/main/java/kakao/tuple/Solution.java
@@ -1,41 +1,23 @@
 package kakao.tuple;
 
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.Comparator;
-import java.util.StringTokenizer;
+import java.util.HashSet;
+import java.util.Set;
 
 class Solution {
     public int[] solution(String s) {
-        StringTokenizer st = new StringTokenizer(s, "{}");
-
-        ArrayList<String> list = new ArrayList<>();
-        while (st.hasMoreTokens()) {
-            String temp = st.nextToken();
-            if (!temp.equals(",")) {
-                list.add(temp);
-            }
-        }
-
-        Collections.sort(list, Comparator.comparingInt(String::length));
-
-
-        ArrayList<String> answerList = new ArrayList<>();
-
-        for (String temp : list) {
-            String[] arr = temp.split(",");
-
-            for (int i = 0; i < arr.length; i++) {
-                if (!answerList.contains(arr[i])) {
-                    answerList.add(arr[i]);
+        Set<String> set = new HashSet<>();
+        String[] arr = s.replaceAll("[{]", " ").replaceAll("[}]", " ").trim().split(" , ");
+        Arrays.sort(arr, Comparator.comparingInt(String::length));
+        int[] answer = new int[arr.length];
+        int idx = 0;
+        for (String s1 : arr) {
+            for (String s2 : s1.split(",")) {
+                if (set.add(s2)) {
+                    answer[idx++] = Integer.parseInt(s2);
                 }
             }
-        }
-
-        int[] answer = new int[answerList.size()];
-
-        for (int i = 0; i < answerList.size(); i++) {
-            answer[i] = Integer.parseInt(answerList.get(i));
         }
 
         return answer;
