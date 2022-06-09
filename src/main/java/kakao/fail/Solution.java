c5bd3bc7e0b7f11b2b8172f063c76535ab013d06
Youngrock Choi <tbvjdudfhr@naver.com>
2022-06-09 20:41:19 +0900

    feat: 실패율

--- src/main/java/kakao/fail/Solution.java
+++ src/main/java/kakao/fail/Solution.java
@@ -1,8 +1,56 @@
 package kakao.fail;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 class Solution {
     public int[] solution(int N, int[] stages) {
-        int[] answer = {};
+        int[] answer = new int[N];
+        List<Stage> arr = new ArrayList<>();
+
+        for (int i = 0; i < N; i++) {
+            int fail = 0;
+            int total = 0;
+
+            for (int j = 0; j < stages.length; j++) {
+                if (stages[j] == i + 1) {
+                    fail++;
+                }
+                if (stages[j] >= i + 1) {
+                    total++;
+                }
+            }
+
+            arr.add(new Stage(i + 1, fail == 0 ? 0.0 : (double) fail / total));
+        }
+        Collections.sort(arr);
+
+        for (int i = 0; i < N; i++) {
+            answer[i] = arr.get(i).n;
+        }
+
         return answer;
+    }
+
+    class Stage implements Comparable<Stage> {
+        int n;
+        double rate;
+
+        public Stage(int n, double rate) {
+            this.n = n;
+            this.rate = rate;
+        }
+
+        @Override
+        public int compareTo(Stage other) {
+            int compareValue = Double.compare(other.rate, this.rate);
+
+            if (compareValue == 0) {
+                return Integer.compare(this.n, other.n);
+            }
+
+            return compareValue;
+        }
     }
 }
