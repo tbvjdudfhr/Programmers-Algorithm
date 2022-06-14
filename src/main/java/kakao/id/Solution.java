e6490dbeec07e0624f8dcfe1295466da26232da9
Youngrock Choi <tbvjdudfhr@naver.com>
2022-06-14 21:21:12 +0900

    refactor: 신규 아이디 추천

--- src/main/java/kakao/id/Solution.java
+++ src/main/java/kakao/id/Solution.java
@@ -1,28 +1,117 @@
 package kakao.id;
 
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 class Solution {
     public String solution(String new_id) {
-        String id = new_id.toLowerCase();
-        id = id.replaceAll("[^-_.a-z0-9]", "");
-        id = id.replaceAll("[.]{2,}", ".");
-        id = id.replaceAll("^[.]|[.]$", "");
+        return NewIdFactory.createFrom(new_id)
+                .getNewId();
+    }
 
-        if(id.equals(""))
-        {
-            id += "a";
-        };
+    private static class NewIdFactory {
+        public static NewId createFrom(String newId) {
+            return new NewId(newId)
+                    .toLowerCase()
+                    .removeInvalidLetters()
+                    .replaceMultipleDots()
+                    .removeSideDotsIfPresent()
+                    .replaceIfEmpty()
+                    .removeOverloads()
+                    .removeBackDotsIfPresent()
+                    .appendInsufficientLetters();
+        }
+    }
 
-        if(id.length() >= 16){
-            id = id.substring(0, 15);
-            id = id.replaceAll("^[.]|[.]$", "");
+    private static class NewId {
+        private static final String REMOVAL_REGEX = "[^a-z0-9\\-_.]";
+        private static final String DEFAULT_ID = "a";
+        private static final Integer MAX_LENGTH = 15;
+        private static final Integer MIN_LENGTH = 3;
+        private final String newId;
+
+        public NewId(String newId) {
+            this.newId = newId;
         }
 
-        if(id.length() <= 2) {
-            while(id.length() < 3) {
-                id += id.charAt(id.length() - 1);
+        public NewId toLowerCase() {
+            return new NewId(newId.toLowerCase());
+        }
+
+        public NewId removeInvalidLetters() {
+            return new NewId(newId.replaceAll(REMOVAL_REGEX, ""));
+        }
+
+        public NewId replaceMultipleDots() {
+            StringBuilder builder = new StringBuilder();
+
+            for (int i = 0, length = newId.length(); i < length; i++) {
+                if (newId.charAt(i) == '.') {
+                    while (i < length && newId.charAt(i) == '.') {
+                        i++;
+                    }
+
+                    builder.append(".");
+                    i--;
+                    continue;
+                }
+
+                builder.append(newId.charAt(i));
             }
+
+            return new NewId(builder.toString());
         }
 
-        return id;
+        public NewId removeSideDotsIfPresent() {
+            return removeFrontDotsIfPresent().removeBackDotsIfPresent();
+        }
+
+        public NewId removeFrontDotsIfPresent() {
+            if (newId.startsWith(".")) {
+                return new NewId(newId.substring(1));
+            }
+
+            return this;
+        }
+
+        public NewId removeBackDotsIfPresent() {
+            if (newId.endsWith(".")) {
+                return new NewId(newId.substring(0, newId.length() - 1));
+            }
+
+            return this;
+        }
+
+        public NewId replaceIfEmpty() {
+            if (newId.isEmpty()) {
+                return new NewId(DEFAULT_ID);
+            }
+
+            return this;
+        }
+
+        public NewId removeOverloads() {
+            if (newId.length() > MAX_LENGTH) {
+                return new NewId(newId.substring(0, MAX_LENGTH));
+            }
+
+            return this;
+        }
+
+        public NewId appendInsufficientLetters() {
+            if (newId.length() < MIN_LENGTH) {
+                String appendedId = IntStream.range(0, MIN_LENGTH - newId.length())
+                        .mapToObj(i -> String.valueOf(newId.charAt(newId.length() - 1)))
+                        .collect(Collectors.joining());
+
+                return new NewId(newId.concat(appendedId));
+            }
+
+            return this;
+        }
+
+        public String getNewId() {
+            return newId;
+        }
     }
 }
