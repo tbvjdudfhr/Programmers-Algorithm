b4629fe25bd43563a02cdb1cb42fc2a26cc1a255
Youngrock Choi <tbvjdudfhr@naver.com>
2022-06-29 23:33:08 +0900

    feat: 압축

--- src/main/java/kakao/compression/Solution.java
+++ src/main/java/kakao/compression/Solution.java
@@ -1,8 +1,31 @@
 package kakao.compression;
 
+import java.util.ArrayList;
+
 public class Solution {
     public int[] solution(String msg) {
-        int[] answer = {};
-        return answer;
+        ArrayList<String> dic = new ArrayList<>();
+        ArrayList<Integer> result = new ArrayList<>();
+
+        for (int i = 0; i < 26; i++) {
+            dic.add(String.valueOf((char) ('A' + i)));
+        }
+
+        for (int i = 0; i < msg.length(); i++) {
+            for (int j = dic.size() - 1; j >= 0; j--) {
+                if (msg.substring(i).startsWith(dic.get(j))) {
+                    i += dic.get(j).length() - 1;
+                    result.add(j + 1);
+                    if (i + 1 < msg.length()) {
+                        dic.add(dic.get(j) + msg.charAt(i + 1));
+                    }
+                    break;
+                }
+            }
+        }
+
+        return result.stream()
+                .mapToInt(i -> i)
+                .toArray();
     }
 }
