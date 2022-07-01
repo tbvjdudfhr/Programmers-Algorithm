f43775c83be142587ef5ef2b54984a7d85917d15
Youngrock Choi <tbvjdudfhr@naver.com>
2022-07-01 22:57:13 +0900

    feat: k진수에서 소수 개수 구하기

--- src/main/java/kakao/primenumber/Solution.java
+++ src/main/java/kakao/primenumber/Solution.java
@@ -2,7 +2,37 @@ package kakao.primenumber;
 
 public class Solution {
     public int solution(int n, int k) {
-        int answer = -1;
+        int answer = 0;
+        String num = Integer.toString(n, k);
+        String[] strings = num.split("0");
+
+        for (String s : strings) {
+            if (s.equals("")) {
+                continue;
+            }
+            long number = Long.parseLong(s);
+            if (isPrime(number)) {
+                answer++;
+            }
+        }
         return answer;
+    }
+
+    public static boolean isPrime(long num) {
+        if (num == 1) {
+            return false;
+        }
+
+        if (num == 2) {
+            return true;
+        }
+
+        for (int i = 2; i <= (int) Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
