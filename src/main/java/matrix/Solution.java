99e9d52c04cc6162c7996e7b5d675b6a51036179
Youngrock Choi <tbvjdudfhr@naver.com>
2022-07-09 22:04:04 +0900

    feat: 행렬 테투리 회전하기

--- src/main/java/matrix/Solution.java
+++ src/main/java/matrix/Solution.java
@@ -1,8 +1,54 @@
 package matrix;
 
 public class Solution {
+    int[][] matrix;
     public int[] solution(int rows, int columns, int[][] queries) {
-        int[] answer = {};
+        matrix = new int[rows][columns];
+        int[] answer = new int[queries.length];
+
+        makeMatrix(rows, columns);
+
+        for(int i = 0; i < queries.length; i++){ 
+            answer[i] = rotate(queries[i]);
+        }
+
         return answer;
+    }
+
+    private int rotate(int[] query) {
+        int r1 = query[0]-1;
+        int c1 = query[1]-1;
+        int r2 = query[2]-1;
+        int c2 = query[3]-1;
+
+        int temp = matrix[r1][c1]; 
+        int min = temp;                 
+        for(int i = r1; i < r2; i++){ 
+            matrix[i][c1] = matrix[i+1][c1];
+            if(min > matrix[i][c1]) min = matrix[i][c1];
+        }
+        for(int i = c1; i < c2; i++){ 
+            matrix[r2][i] = matrix[r2][i+1];
+            if(min > matrix[r2][i]) min = matrix[r2][i];
+        }
+        for(int i = r2; i > r1; i--){ 
+            matrix[i][c2] = matrix[i-1][c2];
+            if(min > matrix[i][c2]) min = matrix[i][c2];
+        }
+        for(int i = c2; i > c1; i--){ 
+            matrix[r1][i] = matrix[r1][i-1];
+            if(min > matrix[r1][i]) min = matrix[r1][i];
+        }
+        matrix[r1][c1+1] = temp; 
+        
+        return min;
+    }
+
+    private void makeMatrix(int rows, int columns) {
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < columns; j++){
+                matrix[i][j] = i* columns + j + 1;
+            }
+        }
     }
 }
