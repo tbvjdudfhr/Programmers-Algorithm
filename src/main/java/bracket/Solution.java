9a66907ee3c753330b18a28c7883e981df89e7b1
Youngrock Choi <tbvjdudfhr@naver.com>
2022-07-13 20:35:45 +0900

    feat: 괄호 회전하기

--- src/main/java/bracket/Solution.java
+++ src/main/java/bracket/Solution.java
@@ -1,8 +1,45 @@
 package bracket;
 
+import java.util.Stack;
+
 public class Solution {
     public int solution(String s) {
-        int answer = -1;
+        int answer = 0;
+        String str = s;
+
+        for (int i = 0; i < s.length(); i++) {
+            str = str.substring(1) + str.charAt(0);
+            if (isCorrect(str)) {
+                answer++;
+            }
+        }
+
         return answer;
+    }
+
+    public boolean isCorrect(String str) {
+        Stack<Character> s = new Stack<>();
+
+        char[] arr = str.toCharArray();
+
+        for (char data : arr) {
+            if (s.isEmpty()) {
+                s.push(data);
+            } else if (s.peek() == '[' && data == ']') {
+                s.pop();
+            } else if (s.peek() == '(' && data == ')') {
+                s.pop();
+            } else if (s.peek() == '{' && data == '}') {
+                s.pop();
+            } else {
+                s.push(data);
+            }
+        }
+
+        if (s.isEmpty()) {
+            return true;
+        }
+
+        return false;
     }
 }
