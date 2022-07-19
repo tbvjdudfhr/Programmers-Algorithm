df1481f6d9dc1e5fe374ca3364aa4c211268033b
Youngrock Choi <tbvjdudfhr@naver.com>
2022-07-19 22:43:08 +0900

    feat: Remove Duplicates from Sorted List II

---
+++ src/main/java/leetcode/binarysearch/_82/Solution.java
@@ -0,0 +1,39 @@
+package leetcode.binarysearch._82;
+
+public class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode sentinel = new ListNode(-1, head);
+        ListNode pred = sentinel;
+        while (head != null) {
+            if (head.next != null && head.val == head.next.val) {
+                while (head.next != null && head.val == head.next.val) {
+                    head = head.next;
+                }
+
+                pred.next = head.next;
+            } else {
+                pred = pred.next;
+            }
+
+            head = head.next;
+        }
+        return sentinel.next;
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
---
+++ src/test/java/leetcode/binarysearch/_82/SolutionTest.java
@@ -0,0 +1,30 @@
+package leetcode.binarysearch._82;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static leetcode.binarysearch._82.Solution.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SolutionTest {
+    private static Stream<Arguments> args() {
+        return Stream.of(
+                Arguments.of(
+                        new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(3, new ListNode(4, new ListNode(4, new ListNode(5))))))),
+                        new ListNode(1, new ListNode(2, new ListNode(5)))
+                )
+        );
+    }
+
+    @DisplayName("정렬된 연결리스트가 주어지면 중복 번호를 제거 테스트")
+    @ParameterizedTest
+    @MethodSource("args")
+    void 정렬된_리스트_중복_제거(ListNode head, ListNode result) {
+        Solution sol = new Solution();
+        assertThat(sol.deleteDuplicates(head)).isEqualTo(result);
+    }
+}
