70855b359a66275db3964f63b02c4271f26e4dbe
Youngrock Choi <tbvjdudfhr@naver.com>
2022-08-14 21:26:57 +0900

    chore: revert commit

--- src/main/java/leetcode/_797/Solution.java
+++ src/main/java/leetcode/_797/Solution.java
@@ -1,9 +1,26 @@
 package leetcode._797;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class Solution {
     public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
-        return null;
+        List<List<Integer>> result = new LinkedList<>();
+        List<Integer> path = new LinkedList<>();
+        path.add(0);
+        dfs(graph, 0, path, result);
+        return result;
+    }
+
+    private void dfs(int[][] graph, int index, List<Integer> path, List<List<Integer>> rst) {
+        if (index == graph.length - 1) {
+            rst.add(new LinkedList<>(path));
+            return;
+        }
+        for (int neighbor : graph[index]) {
+            path.add(neighbor);
+            dfs(graph, neighbor, path, rst);
+            path.remove(path.size() - 1);
+        }
     }
 }
