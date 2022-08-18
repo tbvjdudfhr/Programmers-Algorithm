b07da422148ad6a5d14527ef99528a8a8ebf2a5b
Youngrock Choi <tbvjdudfhr@naver.com>
2022-08-18 23:34:45 +0900

    feat: Letter Combinations of a Phone Number

--- src/main/java/leetcode/_17/Solution.java
+++ src/main/java/leetcode/_17/Solution.java
@@ -1,9 +1,28 @@
 package leetcode._17;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Solution {
+    private static final String[] strings = new String[]{"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+
     public List<String> letterCombinations(String digits) {
-        return null;
+        List<String> ret = new ArrayList<>();
+        if (digits.length() == 0) {
+            return ret;
+        }
+        combination("", digits, 0, ret);
+        return ret;
+    }
+
+    public void combination(String prefix, String digits, int offset, List<String> ret) {
+        if (offset >= digits.length()) {
+            ret.add(prefix);
+            return;
+        }
+        String letters = strings[digits.charAt(offset) - '0'];
+        for (int i = 0; i < letters.length(); i++) {
+            combination(prefix + letters.charAt(i), digits, offset + 1, ret);
+        }
     }
 }
