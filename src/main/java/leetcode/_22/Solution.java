807f0a113c7be6e922148ca4223885226c9c0a6a
Youngrock Choi <tbvjdudfhr@naver.com>
2022-08-20 21:49:11 +0900

    feat: Generate Parentheses

--- src/main/java/leetcode/_22/Solution.java
+++ src/main/java/leetcode/_22/Solution.java
@@ -1,9 +1,33 @@
 package leetcode._22;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Solution {
+    StringBuilder stringBuilder;
+    List<String> answer;
+
     public List<String> generateParenthesis(int n) {
-        return null;
+        stringBuilder = new StringBuilder();
+        answer = new ArrayList<>();
+        dfs(0, 0, 0, n);
+        return answer;
+    }
+
+    public void dfs(int depth, int openCnt, int closeCnt, int n) {
+        if (depth == 2 * n) {
+            answer.add(stringBuilder.toString());
+            return;
+        }
+        if (openCnt < n) {
+            stringBuilder.append('(');
+            dfs(depth + 1, openCnt + 1, closeCnt, n);
+            stringBuilder.setLength(stringBuilder.length() - 1);
+        }
+        if (closeCnt < openCnt) {
+            stringBuilder.append(')');
+            dfs(depth + 1, openCnt, closeCnt + 1, n);
+            stringBuilder.setLength(stringBuilder.length() - 1);
+        }
     }
 }
