87afaa3813008dbbe4168f87d543145295ecf7ec
Youngrock Choi <tbvjdudfhr@naver.com>
2022-08-26 22:32:20 +0900

    feat: Jump Game

--- src/main/java/leetcode/_55/Solution.java
+++ src/main/java/leetcode/_55/Solution.java
@@ -2,6 +2,18 @@ package leetcode._55;
 
 public class Solution {
     public boolean canJump(int[] nums) {
+        int nextMax = 0;
+        for (int i = 0; i < nums.length; i++) {
+            nextMax = Math.max(nextMax, i + nums[i]);
+
+            if (nextMax >= nums.length - 1) {
+                return true;
+            }
+
+            if (nextMax == i) {
+                return false;
+            }
+        }
         return true;
     }
 }
