4c5c08b13766fe2752e99d04ca06695d2a8c70b7
Youngrock Choi <tbvjdudfhr@naver.com>
2022-08-28 21:10:45 +0900

    feat: House Robber II

--- src/main/java/leetcode/_213/Solution.java
+++ src/main/java/leetcode/_213/Solution.java
@@ -2,6 +2,27 @@ package leetcode._213;
 
 public class Solution {
     public int rob(int[] nums) {
-        return 0;
+        if (nums.length == 1) {
+            return nums[0];
+        }
+
+        return Math.max(robHelper(nums, 1, nums.length), robHelper(nums, 0, nums.length - 1));
+    }
+
+    public int robHelper(int[] nums, int si, int ei) {
+        int prev = 0;
+        int prev2 = 0;
+
+        for (int i = si; i < ei; i++) {
+            int noPick = prev;
+            int pick = nums[i] + prev2;
+
+            int curr = Math.max(pick, noPick);
+
+            prev2 = prev;
+            prev = curr;
+        }
+
+        return prev;
     }
 }
