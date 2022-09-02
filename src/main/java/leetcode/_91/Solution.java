750c16678cb5a31e60b425bceab6251960dff401
Youngrock Choi <tbvjdudfhr@naver.com>
2022-09-02 22:20:02 +0900

    feat: Decode Ways

--- src/main/java/leetcode/_91/Solution.java
+++ src/main/java/leetcode/_91/Solution.java
@@ -2,6 +2,28 @@ package leetcode._91;
 
 public class Solution {
     public int numDecodings(String s) {
-        return 0;
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+
+        int n = s.length();
+        int[] dp = new int[n + 1];
+
+        dp[0] = 1;
+
+        for (int i = 1; i <= n; ++i) {
+            for (int j = i - 1; j >= 0 && j > i - 3; --j) {
+                String sub = s.substring(j, i);
+                if (sub.charAt(0) == '0') {
+                    continue;
+                }
+                int number = Integer.parseInt(sub);
+
+                if (number > 0 && number <= 26) {
+                    dp[i] += dp[j];
+                }
+            }
+        }
+        return dp[n];
     }
 }
