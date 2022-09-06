726f3d3c688531d6b51157db2146e5f2ee75b6dd
Youngrock Choi <tbvjdudfhr@naver.com>
2022-09-06 21:25:32 +0900

    feat: Longest Increasing Subsequence

--- src/main/java/leetcode/_300/Solution.java
+++ src/main/java/leetcode/_300/Solution.java
@@ -1,7 +1,26 @@
 package leetcode._300;
 
+import java.util.Arrays;
+
 public class Solution {
     public int lengthOfLIS(int[] nums) {
-        return 0;
+        if (nums == null || nums.length == 0)
+            return 0;
+
+        int[] max = new int[nums.length];
+        Arrays.fill(max, 1);
+
+        int result = 1;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    max[i] = Math.max(max[i], max[j] + 1);
+
+                }
+            }
+            result = Math.max(max[i], result);
+        }
+
+        return result;
     }
 }
