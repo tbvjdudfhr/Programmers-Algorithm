25a589b54efd0b2ee8878640c1f54352ffb3a80b
Youngrock Choi <tbvjdudfhr@naver.com>
2022-09-08 22:33:42 +0900

    feat: Number of Longest Increasing Subsequence

--- src/main/java/leetcode/_673/Solution.java
+++ src/main/java/leetcode/_673/Solution.java
@@ -1,7 +1,33 @@
 package leetcode._673;
 
+import java.util.Arrays;
+
 public class Solution {
     public int findNumberOfLIS(int[] nums) {
-        return 0;
+        int n = nums.length;
+        if (n == 1)
+            return 1;
+        int[] dp = new int[n];
+        dp[0] = 1;
+        int max = 0;
+        int[] cut = new int[n];
+        Arrays.fill(cut, 1);
+        for (int i = 1; i < n; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j] && 1 + dp[j] > dp[i]) {
+                    dp[i] = 1 + dp[j];
+                    cut[i] = cut[j];
+                } else if (nums[i] > nums[j] && 1 + dp[j] == dp[i])
+                    cut[i] += cut[j];
+            }
+            max = Math.max(max, dp[i]);
+        }
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            if (dp[i] == max)
+                cnt += cut[i];
+        }
+        return cnt;
     }
 }
