a30470afef377070c2098b74707800d9738e43fe
Youngrock Choi <tbvjdudfhr@naver.com>
2022-09-10 21:56:15 +0900

    feat: Shuffle an Array

--- src/main/java/leetcode/_384/Solution.java
+++ src/main/java/leetcode/_384/Solution.java
@@ -1,14 +1,33 @@
 package leetcode._384;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class Solution {
+    Random rand;
+    int len;
+    int[] shuffled;
+    int[] original;
+
     public Solution(int[] nums) {
+        rand = new Random();
+        len = nums.length;
+        shuffled = new int[len];
+        original = nums;
     }
 
     public int[] reset() {
-        return null;
+        return original;
     }
 
     public int[] shuffle() {
-        return null;
+        Arrays.fill(shuffled, Integer.MIN_VALUE);
+        for (int i = 0; i < len; ) {
+            int newPos = rand.nextInt(len);
+            if (shuffled[newPos] == Integer.MIN_VALUE) {
+                shuffled[newPos] = original[i++];
+            }
+        }
+        return shuffled;
     }
 }
