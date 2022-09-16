6de208bb695f4c2a21a5e277a6bb98c8f9e0e1ca
Youngrock Choi <tbvjdudfhr@naver.com>
2022-09-16 20:48:59 +0900

    feat: Remove Nth Node From End of List

--- src/main/java/leetcode/_19/Solution.java
+++ src/main/java/leetcode/_19/Solution.java
@@ -2,7 +2,24 @@ package leetcode._19;
 
 public class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        return null;
+        ListNode dummy = new ListNode();
+        dummy.next = head;
+
+        ListNode p1 = dummy;
+        ListNode p2 = dummy;
+
+        for (int i=0; i<n+1; i++) {
+            p1 = p1.next;
+        }
+
+        while (p1 != null) {
+            p1 = p1.next;
+            p2 = p2.next;
+        }
+
+        p2.next = p2.next.next;
+
+        return dummy.next;
     }
 
     public static class ListNode {
