7f7526a8ad47128cb6102f95dedf6800face760e
Youngrock Choi <tbvjdudfhr@naver.com>
2022-09-19 21:17:10 +0900

    test: Odd Even Linked List

---
+++ src/main/java/leetcode/_328/Solution.java
@@ -0,0 +1,24 @@
+package leetcode._328;
+
+public class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        return null;
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
---
+++ src/test/java/leetcode/_328/SolutionTest.java
@@ -0,0 +1,35 @@
+package leetcode._328;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static leetcode._328.Solution.ListNode;
+
+class SolutionTest {
+    private static Stream<Arguments> args() {
+        return Stream.of(
+                Arguments.of(
+                        new ListNode(1, new ListNode(2, new ListNode(2, new ListNode(1)))),
+                        true
+                ),
+                Arguments.of(
+                        new ListNode(1, new ListNode(2)),
+                        false
+                )
+        );
+    }
+
+    @DisplayName("단일 연결 목록의 헤드가 주어지면 홀수 인덱스를 가진 모든 노드를 그룹화하고 짝수 인덱스를 가진 노드를 그룹화하고 재정렬된 목록을 반환 테스트")
+    @ParameterizedTest
+    @MethodSource("args")
+    void 홀수_짝수_연결_리스트(ListNode head, boolean result) {
+        Solution sol = new Solution();
+        Assertions.assertThat(sol.oddEvenList(head)).isEqualTo(result);
+    }
+}
+
