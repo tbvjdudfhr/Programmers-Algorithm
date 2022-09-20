5feaf41ca44b1ca6b52468d0ef1c921cb6d3d051
Youngrock Choi <tbvjdudfhr@naver.com>
2022-09-20 21:17:44 +0900

    feat:  Odd Even Linked List

--- src/main/java/leetcode/_328/Solution.java
+++ src/main/java/leetcode/_328/Solution.java
@@ -2,7 +2,21 @@ package leetcode._328;
 
 public class Solution {
     public ListNode oddEvenList(ListNode head) {
-        return null;
+        if (head == null) return head;
+
+        ListNode odd = head;
+        ListNode even = head.next;
+        ListNode evenHead = even;
+
+        while (odd.next != null && even.next != null) {
+            odd.next = odd.next.next;
+            even.next = even.next.next;
+            odd = odd.next;
+            even = even.next;
+        }
+
+        odd.next = evenHead;
+        return head;
     }
 
     public static class ListNode {
--- src/test/java/leetcode/_328/SolutionTest.java
+++ src/test/java/leetcode/_328/SolutionTest.java
@@ -14,12 +14,8 @@ class SolutionTest {
     private static Stream<Arguments> args() {
         return Stream.of(
                 Arguments.of(
-                        new ListNode(1, new ListNode(2, new ListNode(2, new ListNode(1)))),
-                        true
-                ),
-                Arguments.of(
-                        new ListNode(1, new ListNode(2)),
-                        false
+                        new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5))))),
+                        new ListNode(1, new ListNode(3, new ListNode(5, new ListNode(2, new ListNode(4)))))
                 )
         );
     }
@@ -27,7 +23,7 @@ class SolutionTest {
     @DisplayName("단일 연결 목록의 헤드가 주어지면 홀수 인덱스를 가진 모든 노드를 그룹화하고 짝수 인덱스를 가진 노드를 그룹화하고 재정렬된 목록을 반환 테스트")
     @ParameterizedTest
     @MethodSource("args")
-    void 홀수_짝수_연결_리스트(ListNode head, boolean result) {
+    void 홀수_짝수_연결_리스트(ListNode head, ListNode result) {
         Solution sol = new Solution();
         Assertions.assertThat(sol.oddEvenList(head)).isEqualTo(result);
     }
