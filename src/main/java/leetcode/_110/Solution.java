f2d8ae3c186ea7d8996c50392812c1e20faee36b
Youngrock Choi <tbvjdudfhr@naver.com>
2022-09-30 22:54:40 +0900

    feat: Balanced Binary Tree

--- src/main/java/leetcode/_110/Solution.java
+++ src/main/java/leetcode/_110/Solution.java
@@ -1,8 +1,27 @@
 package leetcode._110;
 
 public class Solution {
+    boolean balance;
+
     public boolean isBalanced(TreeNode root) {
-        return false;
+        height(root);
+        return balance;
+    }
+
+    public int height(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int left = height(root.left);
+        int right = height(root.right);
+
+        if (Math.abs(left - right) > 1) {
+            balance = false;
+        }
+
+        return Math.max(left, right) + 1;
+
     }
 
     public static class TreeNode {
