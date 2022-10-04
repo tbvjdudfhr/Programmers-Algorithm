4fada3b4c2805818ebf85de641cc7d6273b7e2e9
Youngrock Choi <tbvjdudfhr@naver.com>
2022-10-04 21:17:11 +0900

    feat: Validate Binary Search Tree

--- src/main/java/leetcode/_98/Solution.java
+++ src/main/java/leetcode/_98/Solution.java
@@ -2,7 +2,19 @@ package leetcode._98;
 
 public class Solution {
     public boolean isValidBST(TreeNode root) {
-        return true;
+        if (root == null) return true;
+        return dfs(root, null, null);
+    }
+
+    private boolean dfs(TreeNode root, Integer min, Integer max) {
+        if (root == null)
+            return true;
+
+        if ((min != null && root.val <= min) || max != null && root.val >= max) {
+            return false;
+        }
+
+        return dfs(root.left, min, root.val) && dfs(root.right, root.val, max);
     }
 
     public static class TreeNode {
