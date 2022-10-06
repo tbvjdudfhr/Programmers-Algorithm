55d05e5d34d2c26120f72b4ea787b730466fb767
Youngrock Choi <tbvjdudfhr@naver.com>
2022-10-06 23:03:29 +0900

    feat: Path Sum II

--- src/main/java/leetcode/_113/Solution.java
+++ src/main/java/leetcode/_113/Solution.java
@@ -1,10 +1,28 @@
 package leetcode._113;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Solution {
     public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
-        return null;
+        List<List<Integer>> paths = new ArrayList<>();
+        findPaths(root, targetSum, new ArrayList<>(), paths);
+
+        return paths;
+
+    }
+
+    public void findPaths(TreeNode root, int sum, List<Integer> curr, List<List<Integer>> paths) {
+        if (root == null)
+            return;
+        curr.add(root.val);
+        if (root.val == sum && root.left == null && root.right == null) {
+            paths.add(curr);
+            return;
+        }
+
+        findPaths(root.left, sum - root.val, new ArrayList<>(curr), paths);
+        findPaths(root.right, sum - root.val, new ArrayList<>(curr), paths);
     }
 
     public static class TreeNode {
