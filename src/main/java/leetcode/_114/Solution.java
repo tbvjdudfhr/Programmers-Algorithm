ded7fec3616e4bef96e02b789a5f5bd195f4a77b
Youngrock Choi <tbvjdudfhr@naver.com>
2022-10-08 23:35:52 +0900

    fix: Flatten Binary Tree to Linked List

--- src/main/java/leetcode/_114/Solution.java
+++ src/main/java/leetcode/_114/Solution.java
@@ -1,2 +1,39 @@
-package leetcode._114;public class Solution {
+package leetcode._114;
+
+public class Solution {
+
+    public void flatten(TreeNode root) {
+        if (root == null)
+            return;
+        if (root.left != null) {
+            TreeNode leftRightMost = root.left;
+            while (leftRightMost.right != null){
+                leftRightMost = leftRightMost.right;
+            }
+            TreeNode rootRight = root.right;
+            root.right = root.left;
+            leftRightMost.right = rootRight;
+            root.left = null;
+        }
+        flatten(root.right);
+    }
+
+    public static class TreeNode {
+        int val;
+        Solution.TreeNode left;
+        Solution.TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, Solution.TreeNode left, Solution.TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
 }
