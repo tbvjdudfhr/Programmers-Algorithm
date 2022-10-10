7248bce00738876f0c11d52b73c5acd7a41e7f32
youngrock <youngrock.choi@kstec.co.kr>
2022-10-10 22:19:56 +0900

    feat: 최소값 만들기

--- src/main/java/level2/minimum/Solution.java
+++ src/main/java/level2/minimum/Solution.java
@@ -1,8 +1,16 @@
 package level2.minimum;
 
+import java.util.Arrays;
+
 public class Solution {
     public int solution(int[] A, int[] B) {
         int answer = 0;
+        Arrays.sort(A);
+        Arrays.sort(B);
+
+        for (int i = 0; i < A.length; i++) {
+            answer += A[i] * B[A.length - i];
+        }
         return answer;
     }
 }
