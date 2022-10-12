06ad216a549d2efac6b984fa6ffa8cc83edde328
youngrock <youngrock.choi@kstec.co.kr>
2022-10-12 20:42:27 +0900

    feat: 영어 끝말잇기

--- src/main/java/level2/endtoend/Solution.java
+++ src/main/java/level2/endtoend/Solution.java
@@ -1,9 +1,20 @@
 package level2.endtoend;
 
+import java.util.HashSet;
+
 public class Solution {
     public int[] solution(int n, String[] words) {
-        int[] answer = {};
+        int[] answer = {0, 0};
+        HashSet<String> strings = new HashSet<>();
+        strings.add(words[0]);
+        for (int i = 1; i < words.length; i++) {
+            String word = words[i -1];
+            String nextWord = words[i];
+            if(word.charAt(word.length() -1) != nextWord.charAt(0) || !strings.add(nextWord)) {
+                return new int[]{i % n + 1, i / n + 1};
+            }
 
+        }
         return answer;
     }
 }
