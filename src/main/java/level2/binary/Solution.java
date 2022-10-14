72f7ae924ca35ceb93ea6d002116e46748101425
Youngrock Choi <tbvjdudfhr@naver.com>
2022-10-14 21:01:03 +0900

    feat: 이진 변환 반복하기

--- src/main/java/level2/binary/Solution.java
+++ src/main/java/level2/binary/Solution.java
@@ -2,7 +2,16 @@ package level2.binary;
 
 public class Solution {
     public int[] solution(String s) {
-        int[] answer = {};
-        return answer;
+        int count = 0;
+        int sumZeroCount = 0;
+        while(s.length() > 1) {
+            count++;
+            String replaceAll = s.replaceAll("0", "");
+            int zeroCount = s.length() - replaceAll.length();
+            sumZeroCount += zeroCount;
+            s = Integer.toBinaryString(replaceAll.length());
+        }
+
+        return new int[] {count, sumZeroCount};
     }
 }
