a866fc75d6394ec93ad7f35b8569d13696c88195
Youngrock Choi <tbvjdudfhr@naver.com>
2022-10-17 22:11:04 +0900

    fix: 짝지어 제거하기

--- src/main/java/level2/remove/Solution.java
+++ src/main/java/level2/remove/Solution.java
@@ -1,30 +1,16 @@
 package level2.remove;
 
+import java.util.Stack;
+
 public class Solution {
     public int solution(String s) {
-        int answer = 0;
-        int length = s.length();
-        boolean flag = true;
-
-        while (flag) {
-            for (int i = 0; i < length - 1; i++) {
-                char c = s.charAt(i);
-                char nextC = s.charAt(i + 1);
-                flag = false;
-                if (c == nextC) {
-                    String pair = s.substring(i, i + 2);
-                    s = s.replaceAll(pair, "");
-                    length = s.length();
-                    flag = true;
-                    break;
-                }
-            }
-
-            if(s.isEmpty()) {
-                return 1;
-            }
+        Stack<Character> stack = new Stack<>();
+        for (char ch : s.toCharArray()) {
+            if (!stack.isEmpty() && stack.peek() == ch)
+                stack.pop();
+            else
+                stack.push(ch);
         }
-
-        return answer;
+        return stack.isEmpty() ? 1 : 0;
     }
 }
