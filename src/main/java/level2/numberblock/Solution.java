b86e7e52ffc748e60f4055c02f56c15fb0afa116
Youngrock Choi <tbvjdudfhr@naver.com>
2022-10-23 21:31:30 +0900

    feat: 숫자 블록

--- src/main/java/level2/numberblock/Solution.java
+++ src/main/java/level2/numberblock/Solution.java
@@ -2,7 +2,24 @@ package level2.numberblock;
 
 public class Solution {
     public int[] solution(long begin, long end) {
-        int[] answer = {};
+        int first = (int) begin;
+        int last = (int) end;
+        int[] answer = new int[last - first + 1];
+
+        for (int now = first; now < last + 1; now++) {
+            answer[now - first] = 1;
+
+            for (int div = 2; div <= Math.floor(Math.sqrt(now)); div++)
+                if (now % div == 0 && now / div <= 10000000) {
+                    answer[now - first] = now / div;
+                    break;
+                }
+        }
+
+        if (first == 1) {
+            answer[0] = 0;
+        }
+
         return answer;
     }
 }
