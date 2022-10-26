48a0a8e4742f39f0e052ba2291c9adb559b2b71c
Youngrock Choi <tbvjdudfhr@naver.com>
2022-10-26 23:55:07 +0900

    feat: 택배상자

--- src/main/java/level2/courierbox/Solution.java
+++ src/main/java/level2/courierbox/Solution.java
@@ -1,8 +1,37 @@
 package level2.courierbox;
 
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.stream.IntStream;
+
 public class Solution {
     public int solution(int[] order) {
-        int answer = 0;
-        return answer;
+        Deque<Integer> container = new LinkedList<>();
+        IntStream.rangeClosed(1, order.length).forEach(container::push);
+        Deque<Integer> tmpContainer = new LinkedList<>();
+
+        int cnt = 0;
+        while (!container.isEmpty()) {
+            if (order[cnt] != container.getLast()) {
+                if (!tmpContainer.isEmpty() && order[cnt] == tmpContainer.peek()) {
+                    cnt += 1;
+                    tmpContainer.pop();
+                } else {
+                    tmpContainer.push(container.pollLast());
+                }
+            } else {
+                cnt += 1;
+                container.removeLast();
+            }
+        }
+
+        while (!tmpContainer.isEmpty()) {
+            if (order[cnt] == tmpContainer.peek()) {
+                cnt += 1;
+                tmpContainer.pop();
+            } else break;
+        }
+
+        return cnt;
     }
 }
