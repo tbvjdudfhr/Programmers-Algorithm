87eb2742c2d6e94a8f897e14339c05d3aa461a8d
Youngrock Choi <tbvjdudfhr@naver.com>
2022-10-31 21:15:34 +0900

    feat: 멀쩡한 사각형

--- src/main/java/level2/plainsquare/Solution.java
+++ src/main/java/level2/plainsquare/Solution.java
@@ -2,7 +2,27 @@ package level2.plainsquare;
 
 public class Solution {
     public long solution(int w, int h) {
-        long answer = 1;
+        long answer;
+        long weight = w;
+        long height = h;
+
+        long num = gcd(weight, height);
+
+        answer = weight * height - (weight + height - num);
         return answer;
+    }
+
+    public long gcd(long x, long y) {
+        long big = Math.max(x, y);
+        long small = Math.min(x, y);
+
+        long temp = 0;
+        while (small > 0) {
+            temp = big % small;
+            big = small;
+            small = temp;
+        }
+
+        return big;
     }
 }
