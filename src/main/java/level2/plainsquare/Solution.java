38ef432f57faa3458772160c7558b34071aa40e2
Youngrock Choi <tbvjdudfhr@naver.com>
2022-11-01 20:52:53 +0900

    fix: 멀쩡한 사각형

--- src/main/java/level2/plainsquare/Solution.java
+++ src/main/java/level2/plainsquare/Solution.java
@@ -1,28 +1,12 @@
 package level2.plainsquare;
 
+import java.math.BigInteger;
+
 public class Solution {
     public long solution(int w, int h) {
-        long answer;
-        long weight = w;
-        long height = h;
+        long totalCount = (long) w * (long) h;
+        long diagonalCount = w + h - BigInteger.valueOf(w).gcd(BigInteger.valueOf(h)).longValue();
 
-        long num = gcd(weight, height);
-
-        answer = weight * height - (weight + height - num);
-        return answer;
-    }
-
-    public long gcd(long x, long y) {
-        long big = Math.max(x, y);
-        long small = Math.min(x, y);
-
-        long temp = 0;
-        while (small > 0) {
-            temp = big % small;
-            big = small;
-            small = temp;
-        }
-
-        return big;
+        return totalCount - diagonalCount;
     }
 }
