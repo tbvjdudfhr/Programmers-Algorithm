ed27f1da0781176c979df8f3c0147e246ac116ea
Youngrock Choi <tbvjdudfhr@naver.com>
2022-11-10 23:46:08 +0900

    feat: 푸드 파이트 대회

--- src/main/java/level2/foodfight/Solution.java
+++ src/main/java/level2/foodfight/Solution.java
@@ -2,7 +2,15 @@ package level2.foodfight;
 
 public class Solution {
     public String solution(int[] food) {
-        String answer = "";
+        StringBuilder builder = new StringBuilder();
+        for (int i = 1; i < food.length; i++) {
+            int count = food[i] / 2;
+            for (int j = 0; j < count; j++) {
+                builder.append(i);
+            }
+        }
+        String answer = builder.toString();
+        answer += "0" + builder.reverse();
         return answer;
     }
 }
