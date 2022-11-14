36c5f3f318f1cefdf63dd9b53d3768aa571acbba
Youngrock Choi <tbvjdudfhr@naver.com>
2022-11-14 20:50:33 +0900

    feat: 햄버거 만들기

--- src/main/java/level2/makinghamburgers/Solution.java
+++ src/main/java/level2/makinghamburgers/Solution.java
@@ -1,8 +1,17 @@
 package level2.makinghamburgers;
 
+import java.util.Arrays;
+
 public class Solution {
     public int solution(int[] ingredient) {
         int answer = 0;
+        String burger = "1231";
+        String str = Arrays.toString(ingredient).replaceAll("[^0-9]","");
+
+        while (str.indexOf(burger) != -1) {
+            str = str.replace(burger, "");
+            answer++;
+        }
         return answer;
     }
 }
