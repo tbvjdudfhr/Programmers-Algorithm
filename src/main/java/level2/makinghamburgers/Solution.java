bb7b304e756c6e2100ddcb36545c24a19df1963d
Youngrock Choi <tbvjdudfhr@naver.com>
2022-11-15 20:27:56 +0900

    fix: 햄버거 만들기

--- src/main/java/level2/makinghamburgers/Solution.java
+++ src/main/java/level2/makinghamburgers/Solution.java
@@ -1,16 +1,24 @@
 package level2.makinghamburgers;
 
-import java.util.Arrays;
+import java.util.Stack;
 
 public class Solution {
     public int solution(int[] ingredient) {
         int answer = 0;
-        String burger = "1231";
-        String str = Arrays.toString(ingredient).replaceAll("[^0-9]","");
-
-        while (str.indexOf(burger) != -1) {
-            str = str.replace(burger, "");
-            answer++;
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < ingredient.length; i++) {
+            stack.push(ingredient[i]);
+            if (stack.size() >= 4 && stack.peek() == 1) {
+                int size = stack.size();
+                if (stack.get(size - 1) == 1 && stack.get(size - 2) == 3 && stack.get(size - 3) == 2 && stack.get(size - 4) == 1) {
+                    int count = 0;
+                    while (count < 4) {
+                        stack.pop();
+                        count++;
+                    }
+                    answer++;
+                }
+            }
         }
         return answer;
     }
