a7bc3d3f8864e357cd4748d2aa61ab3d1ad849f8
Youngrock Choi <tbvjdudfhr@naver.com>
2022-11-19 19:53:02 +0900

    feat: 기사단원의 무기

--- src/main/java/level2/weapon/Solution.java
+++ src/main/java/level2/weapon/Solution.java
@@ -3,6 +3,29 @@ package level2.weapon;
 public class Solution {
     public int solution(int number, int limit, int power) {
         int answer = 0;
+        for (int i = 0; i < number; i++) {
+            answer += validate(divisorCount(i), limit, power);
+        }
         return answer;
+    }
+
+    private int validate(int count, int limit, int power) {
+        if (count > limit) {
+            return power;
+        }
+        return  count;
+    }
+
+    public static int divisorCount(int num)
+    {
+        int count = 0;
+        int i = 1;
+        while(i <= num)
+        {
+            if(num % i == 0)
+                count += 1;
+            i++;
+        }
+        return count;
     }
 }
