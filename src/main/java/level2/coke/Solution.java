3f0cd5496f7edf06e2382bcba53af8c10d912894
Youngrock Choi <tbvjdudfhr@naver.com>
2022-11-22 21:40:46 +0900

    feat: 콜라 문제

--- src/main/java/level2/coke/Solution.java
+++ src/main/java/level2/coke/Solution.java
@@ -3,6 +3,17 @@ package level2.coke;
 public class Solution {
     public int solution(int a, int b, int n) {
         int answer = 0;
+
+        while (true) {
+            if ( n < a) {
+                break;
+            }
+
+            int bottles = n / a * b;
+            int moreBottles = n % a;
+            answer += bottles;
+            n = bottles + moreBottles;
+        }
         return answer;
     }
 }
