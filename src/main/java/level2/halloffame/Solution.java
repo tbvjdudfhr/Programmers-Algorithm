57fb281287bfb756ca22c518a1def8d1f11fe575
Youngrock Choi <tbvjdudfhr@naver.com>
2022-11-26 21:43:38 +0900

    feat: 명예의 전당

--- src/main/java/level2/halloffame/Solution.java
+++ src/main/java/level2/halloffame/Solution.java
@@ -1,8 +1,21 @@
 package level2.halloffame;
 
+import java.util.PriorityQueue;
+
 public class Solution {
     public int[] solution(int k, int[] score) {
-        int[] answer = {};
+        int[] answer = new int[score.length];
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        for (int i = 0; i < score.length; i++) {
+            queue.add(score[i]);
+
+            if (queue.size() > k) {
+                queue.poll();
+            }
+
+            answer[i] = queue.peek();
+        }
+
         return answer;
     }
 }
