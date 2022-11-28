54d116da5ec4a6145342e95dbf53a7dbea8c2e33
Youngrock Choi <tbvjdudfhr@naver.com>
2022-11-28 19:58:07 +0900

    feat: 귤 고르기

--- src/main/java/level2/tangerine/Solution.java
+++ src/main/java/level2/tangerine/Solution.java
@@ -1,8 +1,28 @@
 package level2.tangerine;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
 public class Solution {
     public int solution(int k, int[] tangerine) {
         int answer = 0;
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+        for (int size : tangerine) {
+            hashMap.put(size, hashMap.getOrDefault(size, 0) + 1);
+        }
+        List<Map.Entry<Integer, Integer>> entryList = new LinkedList<>(hashMap.entrySet());
+        entryList.sort(((o1, o2) -> hashMap.get(o2.getKey()) - hashMap.get(o1.getKey())));
+
+        int sum = 0;
+        for(Map.Entry<Integer, Integer> entry : entryList){
+            sum += entry.getValue();
+            answer++;
+            if(sum >= k) {
+                break;
+            }
+        }
         return answer;
     }
 }
