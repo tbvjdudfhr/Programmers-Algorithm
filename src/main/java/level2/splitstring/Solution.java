7b90a610c4adc17579492cfba36acd558cc370b4
Youngrock Choi <tbvjdudfhr@naver.com>
2022-12-02 20:48:00 +0900

    feat: 문자열 나누기

--- src/main/java/level2/splitstring/Solution.java
+++ src/main/java/level2/splitstring/Solution.java
@@ -3,6 +3,24 @@ package level2.splitstring;
 public class Solution {
     public int solution(String s) {
         int answer = 0;
-        return answer;
+        char init = s.charAt(0);
+        int count = 0;
+        for (char c : s.toCharArray()) {
+            if (count == 0) {
+                init = c;
+            }
+            if (init == c) {
+                count++;
+            } else {
+                count--;
+            }
+            if (count == 0) {
+                answer++;
+            }
+        }
+        if(count > 0) {
+            answer++;
+        }
+        return answer ;
     }
 }
