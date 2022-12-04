72f88d5afe5b003f3f34cd9d719af9d48bf68c91
Youngrock Choi <tbvjdudfhr@naver.com>
2022-12-04 20:53:49 +0900

    feat: 점 찍기

--- src/main/java/level2/dotted/Solution.java
+++ src/main/java/level2/dotted/Solution.java
@@ -3,6 +3,24 @@ package level2.dotted;
 public class Solution {
     public long solution(int k, int d) {
         long answer = 0;
+
+        for (int i = 0; i <= d; i += k) {
+            int yMaxDistance = yPossibleDistance(i, d);
+            answer += yPossibleCount(yMaxDistance, k);
+        }
         return answer;
+    }
+
+    private static int yPossibleDistance(int x, int d) {
+        long xx = (long) Math.pow(x, 2);
+        long dd = (long) Math.pow(d, 2);
+
+        int result = (int) (Math.sqrt(dd - xx));
+        return result;
+    }
+
+    private static int yPossibleCount(int possible, int k) {
+        int y = (possible / k);
+        return y + 1;
     }
 }
