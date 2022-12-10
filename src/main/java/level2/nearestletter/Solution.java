d5f6ea9278310f6a89a07307789d92d816e6adf7
Youngrock Choi <tbvjdudfhr@naver.com>
2022-12-10 20:48:21 +0900

    feat: 가장 가까운 같은 글자

--- src/main/java/level2/nearestletter/Solution.java
+++ src/main/java/level2/nearestletter/Solution.java
@@ -1,8 +1,19 @@
 package level2.nearestletter;
 
+import java.util.HashMap;
+
 public class Solution {
     public int[] solution(String s) {
-        int[] answer = {};
+        int[] answer = new int[s.length()];
+        HashMap<Character, Integer> hashMap = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            answer[i] = -1;
+            if (hashMap.containsKey(c)) {
+                answer[i] = i - hashMap.get(c);
+            }
+            hashMap.put(c, i);
+        }
         return answer;
     }
 }
