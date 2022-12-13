92a4cd14aead2e91f244b4280646987406eadc6b
Youngrock Choi <tbvjdudfhr@naver.com>
2022-12-13 21:32:16 +0900

    feat: 디펜스 게임

--- src/main/java/level2/defencegame/Solution.java
+++ src/main/java/level2/defencegame/Solution.java
@@ -1,8 +1,37 @@
 package level2.defencegame;
 
+import java.util.Collections;
+import java.util.PriorityQueue;
+
 public class Solution {
     public int solution(int n, int k, int[] enemy) {
         int answer = 0;
+
+        if (k >= enemy.length) {
+            return enemy.length;
+        }
+
+        int count = 0;
+        int remain = n;
+        int i;
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>(Collections.reverseOrder());
+        for (i = 0; i < enemy.length; i++) {
+            int current = enemy[i];
+            priorityQueue.add(current);
+            remain -= current;
+
+            if (remain < 0) {
+                if (count < k) {
+                    remain += priorityQueue.poll();
+                    count++;
+                } else {
+                    break;
+                }
+            }
+        }
+
+        answer = i;
+
         return answer;
     }
 }
