27298c68b0aeb25be268b52da8cf9a305f29b39b
Youngrock Choi <tbvjdudfhr@naver.com>
2022-12-15 22:15:26 +0900

    feat: 연속 부분 수열 합의 개수

--- src/main/java/level2/sequencesum/Solution.java
+++ src/main/java/level2/sequencesum/Solution.java
@@ -1,8 +1,24 @@
 package level2.sequencesum;
 
+import java.util.Arrays;
+import java.util.HashSet;
+
 public class Solution {
     public int solution(int[] elements) {
-        int answer = 0;
-        return answer;
+        int[] newElements = new int[elements.length * 2];
+
+        for (int i = 0; i < elements.length; i++) {
+            newElements[i] = newElements[i + elements.length] = elements[i];
+        }
+
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int i = 1; i <= elements.length; i++) {
+            for (int j = 0; j < elements.length; j++) {
+                set.add(Arrays.stream(newElements, j, j + i).sum());
+            }
+        }
+
+        return set.size();
     }
 }
