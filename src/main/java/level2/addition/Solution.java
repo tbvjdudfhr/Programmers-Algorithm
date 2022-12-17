776903335f040653f2ad75d4e2340137d05f0b8c
Youngrock Choi <tbvjdudfhr@naver.com>
2022-12-17 21:07:17 +0900

    feat: 음양 더하기

--- src/main/java/level2/addition/Solution.java
+++ src/main/java/level2/addition/Solution.java
@@ -2,7 +2,17 @@ package level2.addition;
 
 public class Solution {
     public int solution(int[] absolutes, boolean[] signs) {
-        int answer = 123456789;
+        int answer = 0;
+        for (int i = 0; i < absolutes.length; i++) {
+            answer += absolutes[i] * getSign(signs[i]);
+        }
         return answer;
+    }
+
+    private int getSign(boolean sign) {
+        if (sign) {
+            return 1;
+        }
+        return -1;
     }
 }
