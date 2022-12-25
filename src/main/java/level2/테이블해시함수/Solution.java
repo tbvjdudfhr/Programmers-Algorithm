4e40f4b88569dbee9780b98fd2827e931b0752d4
Youngrock Choi <tbvjdudfhr@naver.com>
2022-12-25 21:58:48 +0900

    feat: 테이블 해시 함수

--- src/main/java/level2/테이블해시함수/Solution.java
+++ src/main/java/level2/테이블해시함수/Solution.java
@@ -1,8 +1,24 @@
 package level2.테이블해시함수;
 
+import java.util.Arrays;
+
 public class Solution {
     public int solution(int[][] data, int col, int row_begin, int row_end) {
-        int answer = 0;
-        return answer;
+        Arrays.sort(data, (o1, o2) -> {
+            if (o1[col - 1] == o2[col - 1]) {
+                return Integer.compare(o2[0], o1[0]);
+            }
+            return Integer.compare(o1[col - 1], o2[col - 1]);
+        });
+        int xor = 0;
+        for (int i = row_begin; i <= row_end; i++) {
+            int[] row = data[i - 1];
+            int sum = 0;
+            for (int n : row) {
+                sum += n % i;
+            }
+            xor ^= sum;
+        }
+        return xor;
     }
 }
