52d8b100f6fe459bea8d6f6130cbf5ccc75e11d7
Youngrock Choi <tbvjdudfhr@naver.com>
2022-12-31 20:56:05 +0900

    feat: 혼자 놀기의 달인

--- src/main/java/level2/playingalone/Solution.java
+++ src/main/java/level2/playingalone/Solution.java
@@ -1,8 +1,27 @@
 package level2.playingalone;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 public class Solution {
     public int solution(int[] cards) {
-        int answer = 0;
-        return answer;
+        int n = cards.length;
+        boolean[] visited = new boolean[n];
+        List<Integer> groups = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            int now = i;
+            int cnt = 0;
+            while (!visited[now]) {
+                cnt++;
+                visited[now] = true;
+                now = cards[now] - 1;
+            }
+            groups.add(cnt);
+        }
+        Collections.sort(groups, Comparator.reverseOrder());
+        return (groups.size() == 1) ? 0 : groups.get(0) * groups.get(1);
     }
 }
