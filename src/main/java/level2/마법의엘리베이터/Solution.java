965db1376011fd90906334c7e089ff4879b211d2
Youngrock Choi <tbvjdudfhr@naver.com>
2023-01-03 20:52:50 +0900

    feat: 마법의 엘리베이터

--- src/main/java/level2/마법의엘리베이터/Solution.java
+++ src/main/java/level2/마법의엘리베이터/Solution.java
@@ -2,7 +2,28 @@ package level2.마법의엘리베이터;
 
 public class Solution {
     public int solution(int storey) {
-        int answer = 0;
+        var answer = 0;
+        String number = "0" + storey;
+        int add = 0;
+        for (int i = number.length() - 1; i >= 1; i--) {
+            int c = Character.getNumericValue(number.charAt(i)) + add;
+
+            if (c == 5) {
+                if (number.charAt(i - 1) < 5) {
+                    answer += c;
+                    add = 0;
+                } else {
+                    answer += 10 - c;
+                    add = 1;
+                }
+            } else if (c < 5) {
+                answer += c;
+                add = 0;
+            } else {
+                answer += 10 - c;
+                add = 1;
+            }
+        }
         return answer;
     }
 }
