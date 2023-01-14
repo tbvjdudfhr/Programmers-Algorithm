fb9710f76934dae9e0c0e80858f37c79747a42b9
Youngrock Choi <tbvjdudfhr@naver.com>
2023-01-14 23:04:40 +0900

    feat:바이러스

--- src/main/java/dfsbfs/바이러스/Solution.java
+++ src/main/java/dfsbfs/바이러스/Solution.java
@@ -1,7 +1,33 @@
 package dfsbfs.바이러스;
 
 public class Solution {
+    static boolean[][] graph;
+    static boolean[] visited;
+    static int answer = -1;
+    static int length;
+
     public int solution(int[][] pair) {
-        return 0;
+        length = pair.length;
+        graph = new boolean[length + 1][length + 1];
+        visited = new boolean[length + 1];
+
+        for (int i = 0; i < pair.length; i++) {
+            for (int j = 0; j < pair[i].length; j++) {
+                graph[i][j] = graph[j][i] = true;
+            }
+        }
+
+        dfs(1);
+        return answer;
+    }
+
+    private void dfs(int idx) {
+        visited[idx] = true;
+        answer++;
+        for (int i = 1; i <= length; i++) {
+            if (visited[i] == false && graph[idx][i]) {
+                dfs(i);
+            }
+        }
     }
 }
