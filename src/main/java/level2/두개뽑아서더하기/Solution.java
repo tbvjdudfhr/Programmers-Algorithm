866a1edbe43622262710c71197b724f5abb0225e
Youngrock Choi <tbvjdudfhr@naver.com>
2023-01-18 20:42:49 +0900

    feat: 두개 뽑아서 더하기

--- src/main/java/level2/두개뽑아서더하기/Solution.java
+++ src/main/java/level2/두개뽑아서더하기/Solution.java
@@ -1,8 +1,17 @@
 package level2.두개뽑아서더하기;
 
+import java.util.HashSet;
+
 public class Solution {
     public int[] solution(int[] numbers) {
-        int[] answer = {};
-        return answer;
+        HashSet<Integer> hashSet = new HashSet<>();
+        for (int i = 0; i < numbers.length - 1; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
+                hashSet.add(numbers[i] + numbers[j]);
+            }
+        }
+        return hashSet.stream()
+                .mapToInt(Integer::intValue)
+                .toArray();
     }
 }
