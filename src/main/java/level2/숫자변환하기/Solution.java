3c6dcc6f220fea1a0d95bd1263da6e163518b528
Youngrock Choi <tbvjdudfhr@naver.com>
2023-01-28 23:13:44 +0900

    feat: 숫자 변환하기

--- src/main/java/level2/숫자변환하기/Solution.java
+++ src/main/java/level2/숫자변환하기/Solution.java
@@ -1,8 +1,42 @@
 package level2.숫자변환하기;
 
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Set;
+
 public class Solution {
     public int solution(int x, int y, int n) {
-        int answer = 0;
-        return answer;
+        int cnt = 0;
+        Queue<Integer> q = new LinkedList<>();
+        Set<Integer> hs = new HashSet<>();
+        q.offer(x);
+
+        while (!q.isEmpty()) {
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                if (q.peek() == y) {
+                    return cnt;
+                }
+                helper(q.poll(), y, n, hs, q);
+            }
+            cnt++;
+        }
+        return -1;
+    }
+
+    public static void helper(int num, int y, int n, Set<Integer> hs, Queue<Integer> q) {
+        if (num + n <= y && !hs.contains(num + n)) {
+            hs.add(num + n);
+            q.offer(num + n);
+        }
+        if (num * 2 <= y && !hs.contains(num * 2)) {
+            hs.add(num * 2);
+            q.offer(num * 2);
+        }
+        if (num * 3 <= y && !hs.contains(num * 3)) {
+            hs.add(num * 3);
+            q.offer(num * 3);
+        }
     }
 }
