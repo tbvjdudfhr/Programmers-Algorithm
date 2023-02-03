86d0dadbbdd01d9ce447968fee703c11dd3d9fe2
Youngrock Choi <tbvjdudfhr@naver.com>
2023-02-03 23:31:46 +0900

    feat: 둘만의 암호

--- src/main/java/level2/둘만의암호/Solution.java
+++ src/main/java/level2/둘만의암호/Solution.java
@@ -2,7 +2,18 @@ package level2.둘만의암호;
 
 public class Solution {
     public String solution(String s, String skip, int index) {
-        String answer = "";
+        char[] words = s.toCharArray();
+        for (int i = 0; i < words.length; i++) {
+            for (int j = 0; j < index; j++) {
+                do {
+                    words[i]++;
+                    if (words[i] > 'z') {
+                        words[i] -= 26;
+                    }
+                } while (skip.contains(String.valueOf(words[i])));
+            }
+        }
+        String answer = String.valueOf(words);
         return answer;
     }
 }
