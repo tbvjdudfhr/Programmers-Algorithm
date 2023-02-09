a449280bebb229a6a741453d1892cba92fb41135
Youngrock Choi <tbvjdudfhr@naver.com>
2023-02-09 23:33:11 +0900

    feat: 인사고과

--- src/main/java/level2/인사고과/Solution.java
+++ src/main/java/level2/인사고과/Solution.java
@@ -1,8 +1,40 @@
 package level2.인사고과;
 
+import java.util.Arrays;
+
 public class Solution {
     public int solution(int[][] scores) {
         int answer = 0;
-        return answer;
+        int[] wonho = scores[0];
+
+        Arrays.sort(scores, (x, y) -> {
+            if (x[0] == y[0]) return Integer.compare(x[1], y[1]);
+            else return Integer.compare(y[0], x[0]);
+        });
+
+        int tmpB = Integer.MIN_VALUE;
+        for (int i = 0; i < scores.length; i++) {
+
+            Boolean isDrop = false;
+
+            if (tmpB < scores[i][1]) {
+                tmpB = scores[i][1];
+            } else if (tmpB > scores[i][1]) {
+                isDrop = true;
+            }
+
+            if (isDrop) {
+                if (scores[i][0] == wonho[0] && scores[i][1] == wonho[1]) {
+                    return -1;
+                }
+                continue;
+            }
+
+            if (scores[i][0] + scores[i][1] > wonho[0] + wonho[1]) {
+                answer++;
+            }
+        }
+
+        return answer + 1;
     }
 }
