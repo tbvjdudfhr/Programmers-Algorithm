6ff483acf4d9a093916231066031f32d0ae83f6c
Youngrock Choi <tbvjdudfhr@naver.com>
2023-02-18 21:59:40 +0900

    feat: 카드 뭉치

--- src/main/java/level2/카드뭉치/Solution.java
+++ src/main/java/level2/카드뭉치/Solution.java
@@ -2,7 +2,21 @@ package level2.카드뭉치;
 
 public class Solution {
     public String solution(String[] cards1, String[] cards2, String[] goal) {
-        String answer = "";
-        return answer;
+        int cardIdx1 = 0;
+        int cardIdx1Max = cards1.length;
+        int cardIdx2 = 0;
+        int cardIdx2Max = cards2.length;
+
+        for (int i = 0; i < goal.length; i++) {
+            String curStr = goal[i];
+            if (cardIdx1 < cardIdx1Max && curStr.equals(cards1[cardIdx1])) {
+                cardIdx1++;
+            } else if (cardIdx2 < cardIdx2Max && curStr.equals(cards2[cardIdx2])) {
+                cardIdx2++;
+            } else {
+                return "No";
+            }
+        }
+        return "Yes";
     }
 }
