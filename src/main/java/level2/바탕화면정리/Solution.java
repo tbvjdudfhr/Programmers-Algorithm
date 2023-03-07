05b387c91056223f79772a49b3b11fde81bcc98d
Youngrock Choi <tbvjdudfhr@naver.com>
2023-03-07 22:23:06 +0900

    feat: 바탕화면 정리

--- src/main/java/level2/바탕화면정리/Solution.java
+++ src/main/java/level2/바탕화면정리/Solution.java
@@ -2,7 +2,25 @@ package level2.바탕화면정리;
 
 public class Solution {
     public int[] solution(String[] wallpaper) {
-        int[] answer = {};
-        return answer;
+        int minRow, minCol;
+        int maxRow, maxCol;
+
+        minRow = minCol = Integer.MAX_VALUE;
+        maxRow = maxCol = Integer.MIN_VALUE;
+
+        int row = wallpaper.length;
+        int col = wallpaper[0].length();
+
+        for (int r = 0; r < row; r++) {
+            for (int c = 0; c < col; c++) {
+                if (wallpaper[r].charAt(c) == '#') {
+                    minRow = Math.min(minRow, r);
+                    minCol = Math.min(minCol, c);
+                    maxRow = Math.max(maxRow, r);
+                    maxCol = Math.max(maxCol, c);
+                }
+            }
+        }
+        return new int[]{minRow, minCol, maxRow + 1, maxCol + 1};
     }
 }
