5177c7b2118b829feb6e73249a36ffa12f2739a3
Youngrock Choi <tbvjdudfhr@naver.com>
2023-03-15 21:32:24 +0900

    feat: 덧칠하기

--- src/main/java/level2/덧칠하기/Solution.java
+++ src/main/java/level2/덧칠하기/Solution.java
@@ -3,6 +3,17 @@ package level2.덧칠하기;
 public class Solution {
     public int solution(int n, int m, int[] section) {
         int answer = 0;
+        int max = 0;
+
+        for (int i = 0; i < section.length; i++) {
+            if (section[i] < max) {
+                continue;
+            }
+
+            answer += 1;
+            max = section[i] + m;
+        }
+
         return answer;
     }
 }
