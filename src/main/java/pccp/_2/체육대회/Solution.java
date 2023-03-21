2de150891ba4717f2333f0de12b3a5aa6662eb24
youngrock <youngrock.choi@kstec.co.kr>
2023-03-21 10:46:08 +0900

    feat: 체육대회

--- src/main/java/pccp/_2/체육대회/Solution.java
+++ src/main/java/pccp/_2/체육대회/Solution.java
@@ -1,8 +1,25 @@
 package pccp._2.체육대회;
 
 public class Solution {
+    int answer = 0;
+    static boolean[] isVisit;
     public int solution(int[][] ability) {
-        int answer = 0;
+        isVisit = new boolean[ability.length];
+        dfs(0, 0, ability);
         return answer;
+    }
+
+    private void dfs(int depth, int sum, int[][] ability) {
+        if (depth == ability[0].length) {
+            answer = Math.max(answer, sum);
+            return;
+        }
+        for (int i = 0; i < ability.length; i++) {
+            if (!isVisit[i]) {
+                isVisit[i] = true;
+                dfs(depth + 1, sum + ability[i][depth], ability);
+                isVisit[i] = false;
+            }
+        }
     }
 }
