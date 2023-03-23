1e8d5239fd1610aa5c4f222d558d77f0ac2a5ea1
Youngrock Choi <tbvjdudfhr@naver.com>
2023-03-23 20:24:26 +0900

    feat: 유전법칙

--- src/main/java/pccp/_3/유전법칙/Solution.java
+++ src/main/java/pccp/_3/유전법칙/Solution.java
@@ -2,7 +2,43 @@ package pccp._3.유전법칙;
 
 public class Solution {
     public String[] solution(int[][] queries) {
-        String[] answer = {};
+        String[] answer;
+        int generation;
+        long number;
+
+        answer = new String[queries.length];
+
+        for (int i = 0; i < queries.length; i++) {
+            generation = queries[i][0];
+            number = queries[i][1];
+
+            answer[i] = solve(generation, number);
+        }
+
         return answer;
+    }
+
+    static String solve(int generation, long number) {
+        long upperCaseLastNum;
+        long centerGroupLastNum;
+        String strRoot = "Rr";
+        long tempNum;
+
+        if (generation == 1) {
+            return strRoot;
+        }
+
+        upperCaseLastNum = (long) Math.pow(4, generation - 2);
+        centerGroupLastNum = upperCaseLastNum + (2 * upperCaseLastNum);
+
+        if (number <= upperCaseLastNum) {
+            return "RR";
+        } else if (upperCaseLastNum < number && number <= centerGroupLastNum) {
+            tempNum = number % ((centerGroupLastNum - upperCaseLastNum) / 2) == 0 ?
+                    (centerGroupLastNum - upperCaseLastNum) / 2 : number % ((centerGroupLastNum - upperCaseLastNum) / 2);
+            return solve(generation - 1, tempNum);
+        } else {
+            return "rr";
+        }
     }
 }
