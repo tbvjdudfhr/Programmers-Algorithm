2de0937d2bbca9eb8aa972e7201fdb6ee9052eb6
youngrock <youngrock.choi@kstec.co.kr>
2023-04-10 16:47:29 +0900

    feat: 달기기 경주

--- src/main/java/level2/달리기경주/Solution.java
+++ src/main/java/level2/달리기경주/Solution.java
@@ -1,8 +1,30 @@
 package level2.달리기경주;
 
+import java.util.HashMap;
+import java.util.stream.IntStream;
+
 public class Solution {
+    HashMap<String, Integer> map;
     public String[] solution(String[] players, String[] callings) {
-        String[] answer = {};
-        return answer;
+        map = new HashMap<>();
+        IntStream.range(0, players.length).forEach(it -> map.put(players[it], it));
+
+        for (String calling : callings) {
+            changePlayer(players, calling);
+        }
+        return players;
+    }
+
+    private void changePlayer(String[] players, String calling) {
+        Integer index = map.get(calling);
+        map.put(calling, index -1);
+        if (index == 0) {
+            return;
+        }
+
+        String temp = players[index -1];
+        map.put(temp, index);
+        players[index -1] = players[index];
+        players[index] = temp;
     }
 }
