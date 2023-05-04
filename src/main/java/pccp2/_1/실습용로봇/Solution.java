02a2d4ab7faf7de4eb0d8bac160950c8f08f1a0c
Youngrock Choi <tbvjdudfhr@naver.com>
2023-05-04 22:07:34 +0900

    feat: 실습용 로봇

--- src/main/java/pccp2/_1/실습용로봇/Solution.java
+++ src/main/java/pccp2/_1/실습용로봇/Solution.java
@@ -2,7 +2,32 @@ package pccp2._1.실습용로봇;
 
 public class Solution {
     public int[] solution(String command) {
-        int[] answer = {};
+        int[] answer = new int[2];
+        int[][] path = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        int x = 0;
+        int y = 0;
+        int d = 0;
+        for (char c : command.toCharArray()) {
+            switch (c) {
+                case 'R':
+                    d = (d + 1) % 4;
+                    break;
+                case 'L':
+                    d = (d + 3) % 4;
+                    break;
+                case 'G':
+                    x += path[d][0];
+                    y += path[d][1];
+                    break;
+                case 'B':
+                    x -= path[d][0];
+                    y -= path[d][1];
+                    break;
+            }
+        }
+
+        answer[0] = x;
+        answer[1] = y;
         return answer;
     }
 }
