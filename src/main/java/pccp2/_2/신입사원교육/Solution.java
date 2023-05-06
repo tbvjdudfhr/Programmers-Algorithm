24fac8e8f89caa863080e3c004471447ce8f4c8a
Youngrock Choi <tbvjdudfhr@naver.com>
2023-05-06 21:08:00 +0900

    feat: 신입사원 교육

--- src/main/java/pccp2/_2/신입사원교육/Solution.java
+++ src/main/java/pccp2/_2/신입사원교육/Solution.java
@@ -1,8 +1,23 @@
 package pccp2._2.신입사원교육;
 
+import java.util.PriorityQueue;
+
 public class Solution {
     public int solution(int[] ability, int number) {
-        int answer = 0;
-        return answer;
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int a : ability) {
+            pq.offer(a);
+        }
+        for (int i = 0; i < number; i++) {
+            int first = pq.poll();
+            int second = pq.poll();
+            pq.offer(first + second);
+            pq.offer(first + second);
+        }
+        int sum = 0;
+        for (int a : pq) {
+            sum += a;
+        }
+        return sum;
     }
 }
