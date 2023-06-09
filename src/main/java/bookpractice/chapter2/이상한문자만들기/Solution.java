4829fde8ad1864b280453566afe02c188176fb63
Youngrock Choi <tbvjdudfhr@naver.com>
2023-06-09 22:16:01 +0900

    feat: 이상한 문자 만들기

--- src/main/java/bookpractice/chapter2/이상한문자만들기/Solution.java
+++ src/main/java/bookpractice/chapter2/이상한문자만들기/Solution.java
@@ -2,7 +2,35 @@ package bookpractice.chapter2.이상한문자만들기;
 
 public class Solution {
     public String solution(String s) {
-        String answer = "";
-        return answer;
+        StringBuilder result = new StringBuilder();
+        String[] words = s.split(" ");
+
+        for (int j = 0; j < words.length; j++) {
+            StringBuilder convertedWord = new StringBuilder();
+            String word = words[j];
+
+            for (int i = 0; i < word.length(); i++) {
+                char c = word.charAt(i);
+
+                if (i % 2 == 0) {
+                    convertedWord.append(Character.toUpperCase(c));
+                } else {
+                    convertedWord.append(Character.toLowerCase(c));
+                }
+            }
+
+            result.append(convertedWord);
+
+            if (j < words.length - 1) {
+                result.append(" ");
+            }
+        }
+
+        int trailingSpaces = s.length() - s.stripTrailing().length();
+        for (int k = 0; k < trailingSpaces; k++) {
+            result.append(" ");
+        }
+
+        return result.toString();
     }
 }
