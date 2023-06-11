d7c5e337a6218b24d0224bbebcbe075d482562f2
Youngrock Choi <tbvjdudfhr@naver.com>
2023-06-11 16:21:45 +0900

    feat: 문자열 압축

--- src/main/java/bookpractice/chapter2/문자열압축/Solution.java
+++ src/main/java/bookpractice/chapter2/문자열압축/Solution.java
@@ -2,7 +2,36 @@ package bookpractice.chapter2.문자열압축;
 
 public class Solution {
     public int solution(String s) {
-        int answer = 0;
+        int answer = s.length();
+
+        for (int unit = 1; unit <= s.length() / 2; unit++) {
+            StringBuilder compressed = new StringBuilder();
+            String prev = s.substring(0, unit);
+            int count = 1;
+
+            for (int i = unit; i < s.length(); i += unit) {
+                String curr = (i + unit <= s.length()) ? s.substring(i, i + unit) : s.substring(i);
+                if (prev.equals(curr)) {
+                    count++;
+                } else {
+
+                    if (count > 1) {
+                        compressed.append(count);
+                    }
+                    compressed.append(prev);
+                    prev = curr;
+                    count = 1;
+                }
+            }
+
+            if (count > 1) {
+                compressed.append(count);
+            }
+            compressed.append(prev);
+
+            answer = Math.min(answer, compressed.length());
+        }
+
         return answer;
     }
 }
