5f132b34882a4d9a82906b8f9aebb63bb02c4c67
Youngrock Choi <tbvjdudfhr@naver.com>
2023-06-14 21:35:44 +0900

    feat: 이진 변환 반복하기

--- src/main/java/bookpractice/chapter2/이진변환반복하기/Solution.java
+++ src/main/java/bookpractice/chapter2/이진변환반복하기/Solution.java
@@ -2,7 +2,39 @@ package bookpractice.chapter2.이진변환반복하기;
 
 public class Solution {
     public int[] solution(String s) {
-        int[] answer = {};
-        return answer;
+        int transformCount = 0;
+        int zeroCount = 0;
+
+        while (!s.equals("1")) {
+            int originalLength = s.length();
+            s = binaryConversion(s);
+            int newLength = s.length();
+
+            zeroCount += (originalLength - newLength);
+            transformCount++;
+        }
+
+        return new int[]{transformCount, zeroCount};
+    }
+
+
+    private String binaryConversion(String s) {
+        StringBuilder sb = new StringBuilder();
+
+        for (char c : s.toCharArray()) {
+            if (c != '0') {
+                sb.append(c);
+            }
+        }
+
+        int length = sb.length();
+        sb.setLength(0);
+
+        while (length > 0) {
+            sb.append(length % 2);
+            length /= 2;
+        }
+
+        return sb.reverse().toString();
     }
 }
