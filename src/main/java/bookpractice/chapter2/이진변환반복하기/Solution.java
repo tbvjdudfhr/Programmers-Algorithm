6e5c88ed44a18b600ab0747c9470827186ba2bee
Youngrock Choi <tbvjdudfhr@naver.com>
2023-06-15 19:53:31 +0900

    fix: 이진 변환 반복하기

--- src/main/java/bookpractice/chapter2/이진변환반복하기/Solution.java
+++ src/main/java/bookpractice/chapter2/이진변환반복하기/Solution.java
@@ -2,39 +2,21 @@ package bookpractice.chapter2.이진변환반복하기;
 
 public class Solution {
     public int[] solution(String s) {
-        int transformCount = 0;
-        int zeroCount = 0;
+        int[] answer = new int[2];
+        int convertCount = 0;
+        int removedZeros = 0;
 
         while (!s.equals("1")) {
-            int originalLength = s.length();
-            s = binaryConversion(s);
-            int newLength = s.length();
-
-            zeroCount += (originalLength - newLength);
-            transformCount++;
+            int len = s.length();
+            s = s.replace("0", "");
+            removedZeros += len - s.length();
+            s = Integer.toBinaryString(s.length());
+            convertCount++;
         }
 
-        return new int[]{transformCount, zeroCount};
-    }
+        answer[0] = convertCount;
+        answer[1] = removedZeros;
 
-
-    private String binaryConversion(String s) {
-        StringBuilder sb = new StringBuilder();
-
-        for (char c : s.toCharArray()) {
-            if (c != '0') {
-                sb.append(c);
-            }
-        }
-
-        int length = sb.length();
-        sb.setLength(0);
-
-        while (length > 0) {
-            sb.append(length % 2);
-            length /= 2;
-        }
-
-        return sb.reverse().toString();
+        return answer;
     }
 }
