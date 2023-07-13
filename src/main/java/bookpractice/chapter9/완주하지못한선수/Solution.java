196d1cc2d32585ec24113e62907b4afd8e8cd3ed
Youngrock Choi <tbvjdudfhr@naver.com>
2023-07-13 20:46:16 +0900

    feat: 완주하지 못한 선수

---
+++ src/main/java/bookpractice/chapter9/완주하지못한선수/Solution.java
@@ -0,0 +1,18 @@
+package bookpractice.chapter9.완주하지못한선수;
+
+import java.util.Arrays;
+
+public class Solution {
+    public String solution(String[] participant, String[] completion) {
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+
+        for (int i = 0; i < completion.length; i++) {
+            if (!participant[i].equals(completion[i])) {
+                return participant[i];
+            }
+        }
+
+        return participant[participant.length - 1];
+    }
+}
