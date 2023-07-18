f0bd2c43c3996eb049582e98f42689ba3f051850
Youngrock Choi <tbvjdudfhr@naver.com>
2023-07-18 19:41:14 +0900

    feat: 올바른 괄호

---
+++ src/main/java/bookpractice/chapter11/올바른괄호/Solution.java
@@ -0,0 +1,22 @@
+package bookpractice.chapter11.올바른괄호;
+
+import java.util.Stack;
+
+public class Solution {
+    public boolean solution(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                stack.push(c);
+            } else if (c == ')') {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                stack.pop();
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
